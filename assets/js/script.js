fe412373484450226457d23cadafd56e00b6edf8
Nando <nandodavila94@gmail.com>
2021-09-28 12:51:37 -0400

    realized the starter CSS code had prenamed classes, so adjusted html to fit that, and got colors working just gotta display local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,43 @@
-// var timeBlockTimes = moment().startOf('day').format('h:00 a');
 var moment = moment();
-// var saveBtn = $('.save')
 
 
 var currentDay = moment.format('dddd, MMMM Do YYYY');
 $('#currentDay').text(currentDay);
 
-var currentHour = moment.format('h:00 a');
+var currentHour = moment.hour();
 
 
 $(document).ready(function () {
-    $('.save').on('click', function() {
+    $('.saveBtn').on('click', function() {
         var text = $(this).parent().siblings().children('#form-1').val();
         var time = $(this).parent().parent().attr("id");
         localStorage.setItem(time, text);
         })})
     
+function timeColor() {
+    $('.time-block').each(function () {
+        var timeRow = parseInt($(this).attr("id").split("time")[1]);
+      if (timeRow < currentHour) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+    }
+    else if (timeRow === currentHour) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+    }
+    else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+}})}
 
-
-
+function getLocalInput() { 
+    for (i = 0; i <localStorage.length; i++) {
+        $('.form-1').text('yee yee')
+    }
+}
+console.log(localStorage.getItem('time' + 5))
+timeColor();
+getLocalInput();
