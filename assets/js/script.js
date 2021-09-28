0bff6354c5f70d90c281d14fc5aa0a947244bdec
Nando <nandodavila94@gmail.com>
2021-09-28 10:18:39 -0400

    finally got info storing in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,21 @@
 // var timeBlockTimes = moment().startOf('day').format('h:00 a');
 var moment = moment();
-var saveBtn = $('.save')
+// var saveBtn = $('.save')
 
 
-var currentDay = moment.format('dddd, MMMM YYYY');
+var currentDay = moment.format('dddd, MMMM Do YYYY');
 $('#currentDay').text(currentDay);
 
 var currentHour = moment.format('h:00 a');
 
 
-// function getFormInput () {
-//     var storedForm = localStorage.getItem("time-input");
-//     if (storedForm) {
-//         formLocal = JSON.parse(storedForm);
-//         console.log(formLocal)
-//     };
-// };
-
 $(document).ready(function () {
-    saveBtn.on('click', function(event) {
-        event.preventDefault();
-        var formInput = $('#form-1').val();
-        console.log(formInput)
-        debugger
-        var text = saveBtn.siblings(formInput).val();
-        var time = saveBtn.parent().attr("id");
-        console.log(formInput)
-        if (formInput = null) {
-            alert('nothing to save')
-        } else {
-            localStorage.setItem(time, text);
-        }
-    })
-})
+    $('.save').on('click', function() {
+        var text = $(this).parent().siblings().children('#form-1').val();
+        var time = $(this).parent().parent().attr("id");
+        localStorage.setItem(time, text);
+        })})
+    
 
-// getFormInput();
-// setFormInput();
+
 
