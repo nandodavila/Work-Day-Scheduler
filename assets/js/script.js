364b51ea3288aea6d5421b7a9179d4f54245cc25
Nando <nandodavila94@gmail.com>
2021-09-28 19:58:19 -0400

    I think everything is working, going to double check during the working  hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,10 +34,24 @@ function timeColor() {
 }})}
 
 function getLocalInput() { 
-    for (i = 0; i <localStorage.length; i++) {
-        $('.form-1').text('yee yee')
+    var time5 = $("#time5").children().closest('td').children('#form-1').val(localStorage.getItem("time5"));
+    var time6 = $("#time6").children().closest('td').children('#form-1').val(localStorage.getItem("time6"));
+    var time7 = $("#time7").children().closest('td').children('#form-1').val(localStorage.getItem("time7"));
+    var time8 = $("#time8").children().closest('td').children('#form-1').val(localStorage.getItem("time8"));
+    var time9 = $("#time9").children().closest('td').children('#form-1').val(localStorage.getItem("time9"));
+    var time10 = $("#time10").children().closest('td').children('#form-1').val(localStorage.getItem("time10"));
+    var time11 = $("#time11").children().closest('td').children('#form-1').val(localStorage.getItem("time11"));
+    var time12 = $("#time12").children().closest('td').children('#form-1').val(localStorage.getItem("time12"));
+    var time13 = $("#time13").children().closest('td').children('#form-1').val(localStorage.getItem("time13"));
+    var time14 = $("#time14").children().closest('td').children('#form-1').val(localStorage.getItem("time14"));
+    var time15 = $("#time15").children().closest('td').children('#form-1').val(localStorage.getItem("time15"));
+    var time16 = $("#time16").children().closest('td').children('#form-1').val(localStorage.getItem("time16"));
+    console.log(time5)
+    var allTimes = [time5, time6, time7, time8, time9, time10, time11, time12, time13, time14, time15, time16];
+    for (i = 0; i < allTimes.length; i++) {
+        console.log(allTimes[i].val())
+        $('.form-1').text(allTimes[i].val())
     }
 }
-console.log(localStorage.getItem('time' + 5))
 timeColor();
 getLocalInput();
